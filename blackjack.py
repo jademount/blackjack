5a31d8cb8bfb0828bc35c526ee2fb9a4faab6b85
jademount <linhw1978@gmail.com>
2018-03-20 06:40:55 +0800

    formatted by pycharm

--- blackjack.py
+++ blackjack.py
@@ -2,21 +2,24 @@ import random
 
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
-         'Queen':10, 'King':10, 'Ace':11}
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10,
+          'Jack': 10,
+          'Queen': 10, 'King': 10, 'Ace': 11}
+
 
 class Card:
 
-    def __init__(self,suit,rank):
-        self.suit=suit
-        self.rank=rank
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
 
     def __str__(self):
-        return "%s of %s" %(self.suit, self.rank)
-        #print(self.rank)
+        return "%s of %s" % (self.suit, self.rank)
+        # print(self.rank)
 
-#c=Card(suit=suits[0],rank=ranks[0])
-#print(c)
+
+# c=Card(suit=suits[0],rank=ranks[0])
+# print(c)
 
 class Deck:
 
@@ -24,10 +27,10 @@ class Deck:
         self.deck = []  # start with an empty list
         for suit in suits:
             for rank in ranks:
-                self.deck.append(Card(suit,rank))
+                self.deck.append(Card(suit, rank))
 
     def __str__(self):
-        #Deck.shuffle(self)
+        # Deck.shuffle(self)
         for d in self.deck:
             print(d)
         return "all the cards in this deck"
@@ -36,23 +39,28 @@ class Deck:
         random.shuffle(self.deck)
 
     def deal(self):
+        return self.deck.pop()
+
 
 class Hand:
     def __init__(self):
         self.cards = []  # start with an empty list as we did in the Deck class
-        self.value = 0   # start with zero value
-        self.aces = 0    # add an attribute to keep track of aces
+        self.value = 0  # start with zero value
+        self.aces = 0  # add an attribute to keep track of aces
 
-    def add_card(self,card):
+    def add_card(self, card):
         self.cards.append(card)
 
     def adjust_for_ace(self):
+        x = 0
         for c in self.cards:
-            self.value+=values[c.rank]
-            if c.rank=='Aces':
-                if self.value>21:
-                    self.value-=10
+            self.value += values[c.rank]
+            x += values[c.rank]
+            if c.rank == 'Aces':
+                x = x - 10
+
         return self.value
+
 
 class Chips:
 
@@ -61,80 +69,82 @@ class Chips:
         self.bet = 0
 
     def win_bet(self):
-        self.total+=self.bet
+        self.total += self.bet
 
     def lose_bet(self):
-        self.total-=self.bet
+        self.total -= self.bet
 
-    def take_bet(bets):
-        self.bet=bets
+    def take_bet(self, bets):
+        self.bet = bets
+
 
 def comparedealer(points):
-    if points==21:
+    if points == 21:
         return True
     else:
         return False
 
+
 def printcards(hand):
-    for card in hand.cards[]:
+    for card in hand.cards:
         print(card)
 
 
-
 while True:
-    playerpoint=0
-    dealerpoint=0
-    hityn=""
+    playerpoint = 0
+    dealerpoint = 0
+    hityn = ""
     # Print an opening statement
 
-
     # Create & shuffle the deck, deal two cards to each player
-    deck1=Deck()
+    deck1 = Deck()
     deck1.shuffle()
-    dealerhand=Hand()
+    dealerhand = Hand()
     dealerhand.add_card(deck1.deal())
     dealerhand.add_card(deck1.deal())
-    dealerpoint=dealerhand.adjust_for_ace()
+    dealerpoint = dealerhand.adjust_for_ace()
     print("dealer's hand")
     print("??????????")
-    print(dealerhand.card[1])
-    playerhand=Hand()
+    print(dealerhand.cards[1])
+    playerhand = Hand()
     playerhand.add_card(deck1.deal())
     playerhand.add_card(deck1.deal())
-    playerpoint=playerhand.adjust_for_ace()
+    playerpoint = playerhand.adjust_for_ace()
     print("player's hand")
-    print(playerhand.card[0])
-    print(playerhand.card[1])
+    print(playerhand.cards[0])
+    print(playerhand.cards[1])
     # Set up the Player's chips
-    playerchip=Chips()
+    playerchip = Chips()
 
     # Prompt the Player for their bet
-    playerchip.take_bet(int(input("Player enter your bet please(1~100):")))
+    tobet = 0
+    tobet = int(input("Player enter your bet please(1~100):"))
+    playerchip.take_bet(tobet)
 
     # Show cards (but keep one dealer card hidden)
-    print("dealer's card: ????????? , %s" % dealerhand.cards[1]))
-    print("player's card: %s, %s" %(playerhand.cards[0], playerhand.cards[1]))
-    if playerpoint==21:
-        if dealerpoint==21:
+    print("dealer's card: ????????? , %s" % dealerhand.cards[1])
+    print("player's card: %s, %s" % (playerhand.cards[0], playerhand.cards[1]))
+    if playerpoint == 21:
+        if dealerpoint == 21:
             print('tie, player and dealer both have 21 ')
-            print("dealer's card: %s , %s" %(dealerhand.cards[0],dealerhand.cards[1]))
+            print("dealer's card: %s , %s" % (dealerhand.cards[0], dealerhand.cards[1]))
             print("player has %s chips now" % playerchip.total)
         else:
             print('player wins')
-            print("dealer's card: %s , %s" % (dealerhand.cards[0],dealerhand.cards[1]))
+            print("dealer's card: %s , %s" % (dealerhand.cards[0], dealerhand.cards[1]))
             playerchip.win_bet()
             print("player has %s chips now" % playerchip.total)
     else:
-        while playerpoint<21:
-            hityn=input("Hit?(y or n): ")
-            if hityn=="y":
+        while playerpoint < 21:
+            hityn = input("Hit?(y or n): ")
+            if hityn == "y":
                 playerhand.add_card(deck1.deal())
-                playerpoint=playerhand.adjust_for_ace()
+                playerpoint = playerhand.adjust_for_ace()
                 print("player's card: ")
                 printcards(playerhand)
-                if playerpoint<21:
+                if playerpoint < 21:
                     continue
-                elif playerpoint==21:
+                elif playerpoint == 21:
                     if comparedealer(dealerpoint):
                         print('tie, player and dealer both have 21 ')
                         print("dealer's card: ")
@@ -152,27 +162,27 @@ while True:
                     playerchip.lose_bet()
                     print("player has %s chips now" % playerchip.total)
 
-            elif hityn=="n":
-                while dealerpoint<17 and dealerpoint<playerpoint:
+            elif hityn == "n":
+                while dealerpoint < 17 and dealerpoint < playerpoint:
                     dealerhand.add_card(deck1.deal())
                     printcards(dealerhand)
-                    dealerpoint=dealerhand.adjust_for_ace()
-                if dealerpoint>21:
+                    dealerpoint = dealerhand.adjust_for_ace()
+                if dealerpoint > 21:
                     print("dealer busts!")
                     printcards(dealerhand)
                     playerchip.win_bet()
                     print("player has %s chips now" % playerchip.total)
-                elif dealerpoint==21:
+                elif dealerpoint == 21:
                     print('dealer has blackjack!')
                     playerchip.lose_bet()
                     print("player has %s chips now" % playerchip.total)
-                elif dealerpoint<21 and dealerpoint>playerpoint:
+                elif dealerpoint < 21 and dealerpoint > playerpoint:
                     print('dealer wins')
                     playerchip.lose_bet()
                     print("player has %s chips now" % playerchip.total)
 
-    againyn=input("play again? (y/n)")
-    if againyn=="y":
+    againyn = input("play again? (y/n)")
+    if againyn == "y":
         continue
-    elif againyn=="n":
-        sys.exit()
+    elif againyn == "n":
+        os.exit()
